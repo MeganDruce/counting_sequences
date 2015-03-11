dbf60f118515822eeb4a6f9676d894e84bde42c9
Megan Druce <megdruce26@gmail.com>
2015-03-11 16:26:10 +0200

    Peterexercise

--- bin/count_all_seqs.py
+++ bin/count_all_seqs.py
@@ -1,1 +1,19 @@
-# An empty file
+#!/usr/bin/env python
+
+import sys
+import os
+import seq_utils
+
+def count_seqs(path):
+        filenames = sorted(os.listdir(path))
+        os.chdir(path)
+
+        for filex in filenames:
+                input_file=open(filex)
+                seq_count = seq_utils.count_seqs(input_file)
+                print seq_count, "in", filex
+
+if len(sys.argv) != 2:
+        sys.exit("Usage: count_all_seqs.py <filenames>")
+
+print count_seqs(sys.argv[1])
